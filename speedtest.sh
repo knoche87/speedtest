749f7f4f03405c14f61053669bbabdfac9391d19
knoche87 <55493204+knoche87@users.noreply.github.com>
2021-11-18 22:43:32 +0100

    Update speedtest.sh
    add Ping and clean up curl

--- speedtest.sh
+++ speedtest.sh
@@ -18,17 +18,17 @@ run_speedtest()
     JSON=$(speedtest --accept-license --accept-gdpr -f json)
     DOWNLOAD="$(echo $JSON | jq -r '.download.bandwidth')"
     UPLOAD="$(echo $JSON | jq -r '.upload.bandwidth')"
+    PING="$(echo $JSON | jq -r '.ping.latency')"
     echo "Your download speed is $(($DOWNLOAD  / 125000 )) Mbps ($DOWNLOAD Bytes/s)."
     echo "Your upload speed is $(($UPLOAD  / 125000 )) Mbps ($UPLOAD Bytes/s)."
-
+    echo "Your ping is $PING ms"
+    
     # Save results in the database
     if $DB_SAVE; 
     then
         echo "Saving values to database..."
-        curl -s -S -XPOST "$DB_HOST/write?db=$DB_NAME&precision=s&u=$DB_USERNAME&p=$DB_PASSWORD" \
-            --data-binary "download,host=$HOSTNAME value=$DOWNLOAD $DATE"
-        curl -s -S -XPOST "$DB_HOST/write?db=$DB_NAME&precision=s&u=$DB_USERNAME&p=$DB_PASSWORD" \
-            --data-binary "upload,host=$HOSTNAME value=$UPLOAD $DATE"
+         curl -s -S -XPOST "$DB_HOST/write?db=$DB_NAME&precision=s&u=$DB_USERNAME&p=$DB_PASSWORD" \
+            --data-binary "up-download,host=$HOSTNAME download=$DOWNLOAD,upload=$UPLOAD,ping=$PING $DATE"
         echo "Values saved."
     fi
 }
